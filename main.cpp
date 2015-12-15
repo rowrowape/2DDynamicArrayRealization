afbe783a9b7f8f2602cd3e4896b299a647c8915a
AweMath <dart.fk712@gmail.com>
2015-12-15 22:36:05 +0600

    change function init

--- main.cpp
+++ main.cpp
@@ -10,23 +10,7 @@ struct arr {
     int d1Lower, d1Higher, d2Lower, d2Higher;
 
     arr(int d1Lower, int d1Higher, int d2Lower, int d2Higher, double initial) {
-        this->d1Lower = d1Lower;
-        this->d1Higher = d1Higher;
-        this->d2Lower = d2Lower;
-        this->d2Higher = d2Higher;
 
-        try {
-            base = new double *[d1Higher - d1Lower];
-            for (int i = 0; i < d1Higher - d1Lower; i++) {
-                base[i] = new double[d2Higher - d2Lower];
-            }
-        } catch (bad_alloc) {
-            for (int i = 0; i < d1Higher - d1Lower; i++) {
-                delete base[i];
-            }
-            delete[] base;
-            throw new bad_alloc;
-        }
     }
 };
 
@@ -34,10 +18,22 @@ int init(arr &array, int d1Lower, int d1Higher, int d2Lower, int d2Higher, doubl
     if (d1Higher <= d1Lower || d2Higher <= d2Lower) {
         throw new BadBorderException;
     }
+    array.d1Lower = d1Lower;
+    array.d1Higher = d1Higher;
+    array.d2Lower = d2Lower;
+    array.d2Higher = d2Higher;
+
     try {
-        array = *(new arr(d1Lower, d1Higher, d2Lower, d2Higher, initial));
+        array.base = new double *[d1Higher - d1Lower];
+        for (int i = 0; i < d1Higher - d1Lower; i++) {
+            array.base[i] = new double[d2Higher - d2Lower];
+        }
     } catch (bad_alloc) {
-        throw new MemoryLimitExceded;
+        for (int i = 0; i < d1Higher - d1Lower; i++) {
+            delete array.base[i];
+        }
+        delete[] array.base;
+        throw new BadBorderException;
     }
     return 0;
 }
